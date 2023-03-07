90023bc7af64c4de3ec95efa7307e20e8640d246
Chimera Defi <chimera_defi@protonmail.com>
2023-03-07 01:46:26 -0500

    update stats to show errs and time till duty

--- extra_utils/stats.sh
+++ extra_utils/stats.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+echo "Error scan"
+journalctl -u cl -n 200 | grep error
+journalctl -u eth1 -n 200 | grep error
+journalctl -u validator -n 200 | grep error
+journalctl -u mev -n 200 | grep error
+echo "End error scan output --"
+echo "check time till duty"
+journalctl -u validator -n 1000 | grep timeTillDuty
+echo ''
+echo ''
+echo ''
 
 echo "Versions of clients"
 ../mev-boost/mev-boost -version
